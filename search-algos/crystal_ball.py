17cfc65c91c1d5567f61766cbd42c44a78ee60a4
ice-wzl <75596877+ice-wzl@users.noreply.github.com>
2024-05-07 14:41:33 -0400

    Update crystal_ball.py

--- search-algos/crystal_ball.py
+++ search-algos/crystal_ball.py
@@ -1,3 +1,24 @@
 #!/usr/bin/python3
 
-# you have two crystal balls, 
+# you have two crystal balls, that will break if dropped from high enough
+# distance. determine the exact spot in which it will break in the most 
+# optimized way 
+
+def generate_array(size):
+  arr = []
+  for i in range(0, size):
+      arr.append(i)
+  return arr 
+
+
+
+
+
+
+if __name__ == '__main__':
+    aray = generate_array(100)
+    print(array)
+
+
+
+      
